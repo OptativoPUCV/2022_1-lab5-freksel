4d287904076529aaded1bf2a3c9b7bbf097945b0
Your Name <you@example.com>
2022-04-21 11:55:28 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -43,12 +43,29 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 }
 
 
-void insertTreeMap(TreeMap * tree, void* key, void * value) {
 
+void insertTreeMap(TreeMap * tree, void* key, void * value) {
+    TreeNode* aux=createTreeNode(key,value);
+    Pair* p = searchTreeMap(tree,key);
+        if(p!=NULL){
+        return;
+    }
+    while(aux !=NULL){
+        if(tree->lower_than (key,aux->pair->key)==1){
+            aux=aux->left;
+        }else if(tree->lower_than (aux->pair->key,key)==1){
+            aux=aux->right;
+        }else{
+            if(aux->left==NULL && aux->right==NULL){
+                tree->current=aux;
+                return;
+            }
+        }
+    }
 }
 
 TreeNode * minimum(TreeNode * x){
-
+    
     return NULL;
 }
 
