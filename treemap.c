185f1ab32802795e0004186eb251ccb68f2d8929
Your Name <you@example.com>
2022-04-22 14:14:02 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -168,10 +168,10 @@ Pair * nextTreeMap(TreeMap * tree) {
     else{
         while(aux->parent!=NULL){
             if(tree->lower_than(aux->parent->pair->key,tree->current->pair->key)==1){
-                return aux->parent->pair;
+                aux=aux->parent;
             }
             else if(tree->lower_than(tree->current->pair->key,aux->parent->pair->key)==1){    
-               aux=aux->parent;
+               return aux->parent->pair;;
             }
         }
     }
